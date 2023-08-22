03aa58ead21d722ac472a9ca490f3e7ebc0e1d0e
Éder Xavier <ederxaviervf@gmail.com>
2023-08-22 18:41:03 -0300

    static3

--- projectC/Capp/urls.py
+++ projectC/Capp/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
@@ -11,4 +13,4 @@ urlpatterns = [
     #path('figskic3228863/', views.figskic3228863, name='figskic3228863'),
     
 
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
