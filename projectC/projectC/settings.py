b9560d3c37d7af1a9c499b56be8d7e5ea729c2e1
Éder Xavier <ederxaviervf@gmail.com>
2023-09-22 15:47:48 -0300

    atualizacao4

--- projectC/projectC/settings.py
+++ projectC/projectC/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
 #STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
