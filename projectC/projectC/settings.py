c3e6d40dae49255e0a62eb198bea5ae8ff524999
Éder Xavier <ederxaviervf@gmail.com>
2023-10-19 20:18:51 -0300

    upgrade
    solve the navbar problems

--- projectC/projectC/settings.py
+++ projectC/projectC/settings.py
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 #STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'static')
+#   os.path.join(BASE_DIR, 'static')
 #]
 
 #STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
