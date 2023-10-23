15b9fa2ac8d17d5cab02550357ba8098cb0187fd
Éder Xavier <ederxaviervf@gmail.com>
2023-10-23 19:09:26 -0300

    allowed again

--- projectC/projectC/settings.py
+++ projectC/projectC/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5pwj^hyg)%*i0arxsgqv4a(t-#y=p*a*om^ach$x=3oijcv&$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['projectc-production.up.railway.app']
+
+#ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['projectc-production.up.railway.app']
 
 
