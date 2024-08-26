3987591915a8ca2cc0c97c13ba0834b41e2f644a
Eder Xavier <ederxaviervf@gmail.com>
2024-08-26 15:29:10 -0300

    fix: fix gunicourn timout in lightcurve analysis page

--- projectC/gunicorn_config.py
+++ projectC/gunicorn_config.py
@@ -4,6 +4,10 @@ workers = multiprocessing.cpu_count() * 2 + 1
 bind = 'unix:titan.sock'
 umask = 0o007
 reload = True
+timeout = 600
+graceful_timeout = 600
+max_requests = 1000
+max_requests_jitter = 50
 
 #logging
 accesslog = '-'
